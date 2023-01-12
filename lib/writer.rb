f6a822edf7435f9263c36645b5bc79bf22229c05
Brian Hayes <Brian.hayes1200@gmail.com>
2023-01-12 15:10:54 -0700

    Test: add test for outputing a message

--- lib/writer.rb
+++ lib/writer.rb
@@ -1,6 +1,5 @@
 class Writer
-
-  def initialize 
+  def initialize
     
   end
 end
--- spec/writer_spec.rb
+++ spec/writer_spec.rb
@@ -6,4 +6,11 @@ RSpec.describe Writer do
 
     expect(writer).to be_a(Writer)
   end
+
+  it 'will read a file and puts a message' do 
+    writer = Writer.new
+    
+    expect(writer.message).to eq("sample_message")
+    
+  end
 end
