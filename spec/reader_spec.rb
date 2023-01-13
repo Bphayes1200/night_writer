36b91f1eba21a06f93ac5f6486b6dde7bdfb5fa1
Brian Hayes <Brian.hayes1200@gmail.com>
2023-01-13 12:28:58 -0700

    Test: add test for initialize method for class reader

---
+++ spec/reader_spec.rb
@@ -0,0 +1,17 @@
+require './lib/reader'
+
+RSpec.describe Reader do 
+  it 'will exist' do 
+    braille = 'braille_spec.txt'
+    reader = Reader.new(braille)
+  end
+
+  it 'will read a file and puts a message' do 
+    braille = 'braille_spec.txt'
+    reader = Reader.new(braille)
+    
+    expect(reader.message).to eq(".00.00000.0.
+                                  0.....0.0..0
+                                  0...0.0.0...")
+  end
+end
