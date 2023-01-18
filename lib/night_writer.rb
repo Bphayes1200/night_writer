a3011c65de294adf8bcbd4168e99f9c1748a93df
Brian Hayes <Brian.hayes1200@gmail.com>
2023-01-17 17:44:54 -0700

    Refactor: change formating in reader class

--- lib/night_writer.rb
+++ lib/night_writer.rb
@@ -1,7 +1,7 @@
-require_relative 'writer'
+require_relative 'reader'
 require_relative 'message_converter'
-writer = Writer.new(ARGV[0])
-message = writer.message
+reader = Reader.new(ARGV[0])
+message = reader.message
 message_converter = MessageConverter.new(message)
 message_converter.convert_to_braille
 message_converter.create_new_braille_file(ARGV[1])
